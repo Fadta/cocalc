ee15d44e36ee8f91515b47fd3d5a10c627bd8c28
Fadta <fvpan01@gmail.com>
2021-06-15 20:35:12 -0300

    integrated the parser and error control into the main program

--- __main__.py
+++ __main__.py
@@ -1,8 +1,24 @@
+from parser_ import Parser
 from lexer import Lexer
+from calc_excepts import CocalcException
 
 while True:
-    text = input('cocalc > ')
-    lexer = Lexer(text)
-    tokens = lexer.generate_tokens()
-    print(list(tokens))
+    try:
+        #### Read ####
+        text = input('cocalc > ')
+        lexer = Lexer(text)
+        tokens = lexer.generate_tokens()
+
+        #### Interpret ####
+        parser = Parser(tokens)
+        tree = parser.parse()
+
+        #### Print ####
+        print(tree, end='\n\n')
+    except CocalcException as e:
+        print(e)
+
+    except KeyboardInterrupt:
+        print('Goodbye OwO')
+        exit()
 
